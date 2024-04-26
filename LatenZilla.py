7aa8604911a107ffcf5a44618f2294d8c0030f97
John Punch <cakama3a@gmail.com>
2024-04-26 22:07:56 +0300

    1.0.1

--- LatenZilla.py
+++ LatenZilla.py
@@ -1,5 +1,12 @@
-# LatenZilla 1.0.0
+# LatenZilla 1.0.1
 # Speed Camera Latency Tester by John Punch
+version = "1.0.0"
+print("   ____  _____ ____ _____ _____ _____ ___ ____  ____  _____  ")
+print("  /  _/ /  _  /    /   __/  _  |__   /___/  _/ /  _/ /  _  \ ")
+print("  |  |--|  _  \-  -|   __|  |  |/  _/|   |  |--|  |--|  _  | ")
+print(f"  \_____\__|__/|__|\_____\__|__/_____\___\_____\_____\__|__/  v. {version}")
+print("     Created by https://www.reddit.com/user/JohnnyPunch")
+print()
 
 import pygame
 import threading
@@ -12,19 +19,34 @@ pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 
 if not joysticks:
+    print()
     print("No gamepad found")
-    exit(1)
+    exit(10)
 else:
+    print()
     print(f"Found {len(joysticks)} gamepad(s)")
     for idx, joystick in enumerate(joysticks):
         print(f"{idx + 1}. {joystick.get_name()}")
-    joystick = joysticks[0]
-    joystick.init()
+    print()
+    
+    while True:
+        try:
+            selected_idx = int(input("Select the gamepad index: ")) - 1
+            if 0 <= selected_idx < len(joysticks):
+                joystick = joysticks[selected_idx]
+                break
+            else:
+                print("Invalid index. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a valid index.")
 
+joystick.init()
 start_time = time.time()
 prev_x_value = 0
 prev_y_value = 0
 prev_button_states = [False] * joystick.get_numbuttons()
+
+print()
 
 def timer():
     while True:
@@ -42,24 +64,24 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit(0)
-
+    
     x_axis = joystick.get_axis(0)
     y_axis = joystick.get_axis(1)
-
+    
     if abs(x_axis) >= 0.99 and abs(prev_x_value) < 0.99:
         axis_time = time.time()
         axis_time_ms = int((axis_time - start_time) * 1000)
         sys.stdout.write(f"\rJoystick X-axis reached 0.99 at {axis_time_ms} ms\n")
         sys.stdout.flush()
     prev_x_value = x_axis
-
+    
     if abs(y_axis) >= 0.99 and abs(prev_y_value) < 0.99:
         axis_time = time.time()
         axis_time_ms = int((axis_time - start_time) * 1000)
         sys.stdout.write(f"\rJoystick Y-axis reached 0.99 at {axis_time_ms} ms\n")
         sys.stdout.flush()
     prev_y_value = y_axis
-
+    
     for button_idx in range(joystick.get_numbuttons()):
         button_state = joystick.get_button(button_idx)
         if button_state and not prev_button_states[button_idx]:
@@ -68,5 +90,5 @@ while True:
             sys.stdout.write(f"\rButton {button_idx} pressed at {button_time_ms} ms\n")
             sys.stdout.flush()
         prev_button_states[button_idx] = button_state
-
+    
     time.sleep(0.001)
